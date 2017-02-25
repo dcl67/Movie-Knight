b70e6e3386b8dc2d9ad099d8eab10d5f5c3cb6b3
Danny Lopez <dannylopez@n3-65-209.dhcp.drexel.edu>
2017-02-25 14:13:35 -0500

    html template added.

--- mvindex/models.py
+++ mvindex/models.py
@@ -3,8 +3,11 @@ from django.db import models
 from django.forms import ModelForm
 
 class Movie(models.Model):
-
-    movie_name = models.CharField(max_length=100, blank=True, null=True)
+    imdbid = models.CharField(max_length=100, blank=True, null=True)
+    title = models.CharField(max_length=100, blank=True, null=True)
+    year = models.Charfield(max_length=100, blank=True, null=True)
+    rating = models.ForeignKey('Rating', blank=True, null=True)
+    Runtime = models.
 
     def __str__(self):
-        return self.movie
+        return self.movie_name
