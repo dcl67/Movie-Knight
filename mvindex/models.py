feaf192070ee6c6d83664eb50d1e70aae96070c4
Danny Lopez <dannylopez@n3-65-209.dhcp.drexel.edu>
2017-02-25 12:49:28 -0500

    the website works!

--- mvindex/models.py
+++ mvindex/models.py
@@ -1,5 +1,10 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.forms import ModelForm
 
-# Create your models here.
+class Movie(models.Model):
+
+    movie_name = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.movie
--- mvindex/views.py
+++ mvindex/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from .models import Movie
+
 # Create your views here.
+def index(request):
+    movie = Movie.objects.all()
+    context = {
+        'movie':movie,
+    }
+    return render(request, 'mvindex/index.html', context)
