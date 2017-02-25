ce8a7a845acdaf475d5415a895dd973f5e04451d
Danny Lopez <dannylopez@n3-65-209.dhcp.drexel.edu>
2017-02-25 14:22:38 -0500

    fixed a hanging line

--- mvindex/models.py
+++ mvindex/models.py
@@ -2,12 +2,15 @@ from __future__ import unicode_literals
 from django.db import models
 from django.forms import ModelForm
 
+class Rating(models.Model):
+    rating = models.CharField(max_length=5, blank=True, null=True)
+
 class Movie(models.Model):
     imdbid = models.CharField(max_length=100, blank=True, null=True)
     title = models.CharField(max_length=100, blank=True, null=True)
-    year = models.Charfield(max_length=100, blank=True, null=True)
+    year = models.CharField(max_length=100, blank=True, null=True)
     rating = models.ForeignKey('Rating', blank=True, null=True)
-    Runtime = models.
+    #Runtime = models.
 
     def __str__(self):
         return self.movie_name
